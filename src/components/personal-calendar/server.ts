d800fc9b55d63c720c8ed7ec301d8f6f500be489
Emil Due Rasmussen <emildr12@gmail.com>
2026-03-01 22:28:40 +0100

    Personal ical

--- src/components/personal-calendar/server.ts
+++ src/components/personal-calendar/server.ts
@@ -1,32 +1,49 @@
 import { createServerFn } from "@tanstack/react-start";
 import ICAL from "ical.js";
 
-import type { CalendarEvent } from "./types";
-import { requireEnv } from "@/lib/env";
+import { getEnv, requireEnv } from "@/lib/env";
 
+import type { CalendarEvent } from "./types";
+
+async function getCalendarEventsFromURL(
+  url: string
+): Promise<Array<CalendarEvent>> {
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error(
+      `Failed to fetch calendar: ${res.status} ${res.statusText}`
+    );
+  }
+  const text = await res.text();
+  const jcal = ICAL.parse(text);
+  const comp = new ICAL.Component(jcal);
+  const events = comp.getAllSubcomponents("vevent");
+  return events.map(vevent => {
+    const event = new ICAL.Event(vevent);
+    return {
+      summary: event.summary,
+      start: event.startDate.toJSDate(),
+      end: event.endDate.toJSDate(),
+      location: event.location,
+      description: event.description,
+    };
+  }) as Array<CalendarEvent>;
+}
 
 export const getCalendarEvents = createServerFn({ method: "GET" }).handler(
   async () => {
-    const icalUrl = requireEnv("ICAL_URL");
-    const res = await fetch(icalUrl);
-    if (!res.ok) {
-      throw new Error(
-        `Failed to fetch calendar: ${res.status} ${res.statusText}`
-      );
-    }
-    const text = await res.text();
-    const jcal = ICAL.parse(text);
-    const comp = new ICAL.Component(jcal);
-    const events = comp.getAllSubcomponents("vevent");
-    return events.map(vevent => {
-      const event = new ICAL.Event(vevent);
-      return {
-        summary: event.summary,
-        start: event.startDate.toJSDate(),
-        end: event.endDate.toJSDate(),
-        location: event.location,
-        description: event.description,
-      };
-    }) as Array<CalendarEvent>;
+    const workIcalUrl = requireEnv("ICAL_URL");
+    const personalIcalUrl = getEnv("PERSONAL_ICAL_URL");
+
+    const [workEvents, personalEvents] = await Promise.all([
+      getCalendarEventsFromURL(workIcalUrl),
+      personalIcalUrl === undefined
+        ? Promise.resolve([])
+        : getCalendarEventsFromURL(personalIcalUrl),
+    ]);
+
+    return [...workEvents, ...personalEvents].sort(
+      (a, b) => a.start.getTime() - b.start.getTime()
+    );
   }
 );
